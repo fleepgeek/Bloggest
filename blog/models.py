60d5f4e0b73da26480efef2856ffd1feb5cd1557
emmanuelokiche <fleepgeek@gmail.com>
2018-02-10 16:17:50 +0100

    First Commit

---
+++ blog/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.template.defaultfilters import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    creator = models.ForeignKey(User)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('post_by_category', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, help_text="Slug will be automatically generated from the post's title")
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    view_count = models.PositiveIntegerField(default=0)
+    author = models.ForeignKey(User)
+    category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.id, self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+
---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    
+]
