469a3c747d77d5946cc860f268a1246990fe5947
emmanuelokiche <fleepgeek@gmail.com>
2018-02-12 13:16:09 +0100

    Updated admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Post, Category
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'author', 'category',)
+    readonly_fields = ('slug', 'view_count',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creator',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
