4660ed4fd076d4e2b7f8ecaa015109f6446cf643
emmanuelokiche <fleepgeek@gmail.com>
2018-02-15 14:27:08 +0100

    Implemented home, detail and create views

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
-from django.views.generic import TemplateView
+from blog import views
 
 urlpatterns = [
-    
+    url(r'^post/new/$', views.PostCreate.as_view(), name='post_new'),
+    url(r'^post/(?P<pk>[\d]+)/$', views.PostDetail.as_view(), name='post_detail'),
+    url(r'^$', views.Home.as_view(), name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+from .models import Post
+from .forms import PostForm
+
+class Home(View):
+    def get(self, request):
+        posts = Post.objects.all()
+        return render(request, 'blog/home.html', {'posts': posts})
+
+
+class PostDetail(View):
+    def get(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        return render(request, 'blog/post_detail.html', {'post': post})
+
+
+class PostCreate(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_form.html', {'form': form})
+
+    def post(self, request):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        return render(request, 'blog/post_form.html', {'form': form})
