afd452479b59d752224568e212f10e4fa54a45ad
emmanuelokiche <fleepgeek@gmail.com>
2018-02-19 16:31:18 +0100

    Implemented Crud

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
-from django.views.generic import TemplateView
+
+from blog import views
 
 urlpatterns = [
-    
+    url(r'^dashboard/$', views.Dashboard.as_view(), name='dashboard'),
+    url(r'^post/(?P<pk>[\d]+)/delete/$', views.PostDelete.as_view(), name='post_delete'),
+    url(r'^post/(?P<pk>[\d]+)/update/$', views.PostUpdate.as_view(), name='post_update'),
+    url(r'^post/add/$', views.PostCreate.as_view(), name='post_add'),
+    url(r'^post/(?P<pk>[\d]+)/$', views.PostDetail.as_view(), name='post_detail'),
+    url(r'^$', views.Home.as_view(), name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Post
+
+class Home(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering =  '-pub_date'
+    paginate_by = 3
+
+
+class Dashboard(View):
+    def get(self, request, *args, **kwargs):
+        view = Home.as_view(
+            template_name = 'blog/admin_page.html',
+            paginate_by = 4
+        )
+        return view(request, *args, **kwargs)
+
+
+class PostDetail(DetailView):
+    model = Post
+    def get_object(self):
+        object = super(PostDetail, self).get_object()
+        object.view_count += 1
+        object.save()
+        return object
+
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ('title', 'category', 'content')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.save()
+        return super(PostCreate, self).form_valid(form)
+
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ('title', 'category', 'content')
+
+
+class PostDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('dashboard')
