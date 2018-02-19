5a1a84ed2a97a9131a7c24a885034389a5519fad
emmanuelokiche <fleepgeek@gmail.com>
2018-02-19 17:37:20 +0100

    Created a new Post Detail

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views import View
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from .models import Post, Comment
+from .forms import CommentForm
 
 class Home(ListView):
     model = Post
@@ -22,14 +23,44 @@ class Dashboard(View):
         return view(request, *args, **kwargs)
 
 
-class PostDetail(DetailView):
+class PostDisplay(DetailView):
     model = Post
     def get_object(self):
-        object = super(PostDetail, self).get_object()
+        object = super(PostDisplay, self).get_object()
         object.view_count += 1
         object.save()
         return object
     
+    def get_context_data(self, **kwargs):
+        context = super(PostDisplay, self).get_context_data(**kwargs)
+        context['comments'] = Comment.objects.filter(post=self.get_object())
+        context['form'] = CommentForm
+        return context
+
+
+class PostComment(FormView):
+    form_class = CommentForm
+    template_name = 'blog/post_detail.html'
+
+    def form_valid(self, form):
+        form.instance.by = self.request.user
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        form.instance.post = post
+        form.save()
+        return super(PostComment, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('post_detail', kwargs={'pk': self.kwargs['pk']})
+
+
+class PostDetail(View):
+    def get(self, request, *args, **kwargs):
+        view = PostDisplay.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = PostComment.as_view()
+        return view(request, *args, **kwargs)
 
 
 class PostCreate(CreateView):
