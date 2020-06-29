dd3612ebba0c5a4d96d7063ebe0e3b1790e2512a
fleepgeek <fleepgeek@gmail.com>
2020-06-29 19:17:17 +0100

    initial django 3 setup

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    creator = models.ForeignKey(User)
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         verbose_name_plural = "Categories"
@@ -23,8 +23,8 @@ class Post(models.Model):
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     view_count = models.PositiveIntegerField(default=0)
-    author = models.ForeignKey(User)
-    category = models.ForeignKey(Category)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
@@ -35,6 +35,6 @@ class Post(models.Model):
 
 class Comment(models.Model):
     content = models.TextField()
-    by = models.ForeignKey(User)
-    post = models.ForeignKey(Post)
+    by = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
